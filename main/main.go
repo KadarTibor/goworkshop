e06c75f574693f2468244045e2d4f1b92d66ed0a
kdrtibor <kadartibor24@gmail.com>
2018-02-26 19:35:31 +0200

    serialisez both authors and books

--- main/main.go
+++ main/main.go
@@ -25,28 +25,51 @@ func main() {
 	fmt.Println(book)
 
 
-	fileContent, err := ioutil.ReadFile("model/books.json")
+	booksFileContent, err := ioutil.ReadFile("model/books.json")
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("here")
-	fmt.Println(string(fileContent))
+	fmt.Println(string(booksFileContent))
 
-	var books []model.BookDto
-	err = json.Unmarshal(fileContent, &books)
+	err = json.Unmarshal(booksFileContent, &model.Books)
 	if err != nil {
 		panic(err)
 	}
 
 	fmt.Println("The deserialized books are: ")
-	fmt.Println(books)
+	fmt.Println(model.Books)
 
-	serializedData, err := json.Marshal(books)
+	serializedBooksData, err := json.Marshal(model.Books)
 	if err != nil{
 		panic(err)
 	}
 
 	fmt.Println("The serialized books are:")
-	fmt.Println(string(serializedData))
+	fmt.Println(string(serializedBooksData))
+
+	//////////////////////////////////////
+	authorsFileContent, err := ioutil.ReadFile("model/authors.json")
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println(string(authorsFileContent))
+
+	err = json.Unmarshal(authorsFileContent, &model.Authors)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Println("The deserialized Authors are: ")
+	fmt.Println(model.Authors)
+
+	serializedAuthorsData, err := json.Marshal(model.Authors)
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Println("The serialized Authors are:")
+	fmt.Println(string(serializedAuthorsData))
 }
