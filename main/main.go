b5653d6d4753d518b7a382376f2600c0b303e4b6
Alexandru Grigor <alex.grigor92@gmail.com>
2018-03-05 18:58:55 +0200

    step 8
    (cherry picked from commit f38677209bae91f7810343cc46abc07824806dec)
    Signed-off-by: KadarTibor <kadartibor24@gmail.com>

--- main/main.go
+++ main/main.go
@@ -2,77 +2,18 @@ package main
 
 import (
 	"fmt"
+	"goworkshop/importer"
 	"goworkshop/model"
-	"goworkshop/test"
-	"io/ioutil"
-	"encoding/json"
 	"goworkshop/web"
+	"goworkshop/persistence"
 )
 
 func main() {
-	var s = test.CreateSquare(11)
-	s.Color = "green"
-	fmt.Println(s.Area())
-	fmt.Println(s)
-
-	var book = model.AuthorDto{
-		UUID:      "1234",
-		FirstName: "Ion",
-		LastName:  "Creanga",
-		Birthday:  "1850",
-		Death:     "1914",
-	}
-
-	fmt.Println(book)
-
-
-	booksFileContent, err := ioutil.ReadFile("model/books.json")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("here")
-	fmt.Println(string(booksFileContent))
-
-	err = json.Unmarshal(booksFileContent, &model.Books)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("The deserialized books are: ")
-	fmt.Println(model.Books)
-
-	serializedBooksData, err := json.Marshal(model.Books)
-	if err != nil{
-		panic(err)
-	}
-
-	fmt.Println("The serialized books are:")
-	fmt.Println(string(serializedBooksData))
-
-	//////////////////////////////////////
-	authorsFileContent, err := ioutil.ReadFile("model/authors.json")
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println(string(authorsFileContent))
-
-	err = json.Unmarshal(authorsFileContent, &model.Authors)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Println("The deserialized Authors are: ")
-	fmt.Println(model.Authors)
-
-	serializedAuthorsData, err := json.Marshal(model.Authors)
-	if err != nil{
-		panic(err)
-	}
-
-	fmt.Println("The serialized Authors are:")
-	fmt.Println(string(serializedAuthorsData))
-
+	persistence.InitDB()
+	model.Authors = importer.ImportAuthors()
+	fmt.Printf("Imported authors are: %s\n", model.Authors)
+	model.Books = importer.ImportBooks()
+	fmt.Printf("Imported books are: %s\n", model.Books)
 	web.StartServer()
 }
+
--- web/webserver.go
+++ web/webserver.go
@@ -1,90 +1,38 @@
 package web
 
 import (
-"net/http"
-"os"
-"fmt"
-"goworkshop/model"
-	"encoding/json"
-	"strings"
+	"net/http"
+	"os"
 	"github.com/gorilla/mux"
-	"io/ioutil"
+	"fmt"
 )
 
 const API_PORT_NAME = "API_PORT"
 const API_PORT_VALUE = "8000"
 
 func StartServer() {
-	gorillaMux := mux.NewRouter()
-	http.HandleFunc("/books", httpHandler)
-	http.HandleFunc("/authors/",getAuthorByUUID)
+	router := mux.NewRouter()
+	for _, route := range routes {
+		handlerFunc := log(route.HandlerFunc)
+		router.HandleFunc(route.Pattern, handlerFunc).Methods(route.Method)
+	}
 	var port = getPort()
-	fmt.Println("+-------------------------------+")
-	fmt.Printf("| Starting sever on port: %s\t|\n", port)
-	fmt.Println("+-------------------------------+")
-	if err := http.ListenAndServe(":"+port, nil); err != nil {
+	if err := http.ListenAndServe(":" + port, router); err != nil {
 		panic(err)
 	}
 }
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		serializedBooksData, err := json.Marshal(model.Books)
-		if err != nil{
-			panic(err)
-		}
-		fmt.Fprintln(w, string(serializedBooksData))
-		break
-
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Not supported method!")
-	}
-
-}
-
-func getAuthorByUUID(w http.ResponseWriter, r *http.Request){
-	path := r.URL.Path  //author/UUId
-
-	uuid :=	strings.TrimPrefix(path,"/authors/")
-
-	found := false
-	for _, author := range model.Authors {
-		if author.UUID == uuid{
-			serializedBooksData, err := json.Marshal(author)
-			if err != nil{
-				panic(err)
-			}
-			fmt.Fprintln(w, string(serializedBooksData))
-			found = true
-			}
-	}
-
-	w.WriteHeader(http.StatusNotFound)
-	if(!found){
-		fmt.Fprintln(w,"The author with the UUID %s does not exist", uuid)
-	}
-
-}
-
-func CreateAuthor(w http.ResponseWriter, r *http.Request){
-	body, err := ioutil.ReadAll(r.Body)
-	if(err!= nil){
-		fmt.Fprintf(w, "{\"errorMessage\":\"%s\"}", err.Error())
-		return
-	} else {
-			//TODO
+func log(funcHandler http.HandlerFunc) http.HandlerFunc{
+	fmt.Println("Returning the function")
+	return func (rw http.ResponseWriter, r *http.Request){
+		fmt.Println("New REST request to url: "+r.URL.Path)
+		funcHandler(rw, r)
+		fmt.Println("Rest request ended")
 	}
 }
 
-
-//TODO
-//implement all the methods for author and book
 func getPort() string {
-	port := os.Getenv(API_PORT_NAME)
+	var port = os.Getenv(API_PORT_NAME)
 	if port != "" {
 		return port
 	} else {
