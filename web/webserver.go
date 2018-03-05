43ccacbf66f2e36c00b2307befcd808f3a70764f
KadarTibor <kadartibor24@gmail.com>
2018-03-05 19:28:59 +0200

    Merge branch 'master' of https://github.com/KadarTibor/goworkshop into implement-rest-services
    # Conflicts:
    #	web/webserver.go

--- web/webserver.go
+++ web/webserver.go
@@ -1,14 +1,10 @@
 package web
 
 import (
-"net/http"
-"os"
-"fmt"
-"goworkshop/model"
-	"encoding/json"
-	"strings"
+	"net/http"
+	"os"
+	"fmt"
 	"github.com/gorilla/mux"
-	"io/ioutil"
 )
 
 const API_PORT_NAME = "API_PORT"
@@ -24,67 +20,20 @@ func StartServer() {
 	fmt.Println("+-------------------------------")
 	fmt.Printf("| Starting sever on port: %s\t|\n", port)
 	fmt.Println("-------------------------------+")
-	if err := http.ListenAndServe(":" + port, router); err != nil {
+	if err := http.ListenAndServe(":"+port, router); err != nil {
 		panic(err)
 	}
 }
 
-func httpHandler(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "application/json")
-
-	switch r.Method {
-	case "GET":
-		serializedBooksData, err := json.Marshal(model.Books)
-		if err != nil{
-			panic(err)
-		}
-		fmt.Fprintln(w, string(serializedBooksData))
-		break
-
-	default:
-		w.WriteHeader(http.StatusMethodNotAllowed)
-		fmt.Fprintln(w, "Not supported method!")
-	}
-
-}
-
-func getAuthorByUUID(w http.ResponseWriter, r *http.Request){
-	path := r.URL.Path  //author/UUId
-
-	uuid :=	strings.TrimPrefix(path,"/authors/")
-
-	found := false
-	for _, author := range model.Authors {
-		if author.UUID == uuid{
-			serializedBooksData, err := json.Marshal(author)
-			if err != nil{
-				panic(err)
-			}
-			fmt.Fprintln(w, string(serializedBooksData))
-			found = true
-			}
-	}
-
-func log(funcHandler http.HandlerFunc) http.HandlerFunc{
+func log(funcHandler http.HandlerFunc) http.HandlerFunc {
 	fmt.Println("Returning the function")
-	return func (rw http.ResponseWriter, r *http.Request){
+	return func(rw http.ResponseWriter, r *http.Request) {
 		fmt.Println("New REST request " + r.URL.Path)
 		funcHandler(rw, r)
 		fmt.Println("REST request ended")
 	}
 
 }
-
-func CreateAuthor(w http.ResponseWriter, r *http.Request){
-	body, err := ioutil.ReadAll(r.Body)
-	if(err!= nil){
-		fmt.Fprintf(w, "{\"errorMessage\":\"%s\"}", err.Error())
-		return
-	} else {
-			//TODO
-	}
-}
-
 
 //TODO
 //implement all the methods for author and book
